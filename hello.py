ddc82a478fcdfc6b66a76cc579187cd7878b099c
Geert Jan Bex <geertjan.bex@gmail.com>
2024-02-21 16:14:32 +0100

    Structure code with functions

--- hello.py
+++ hello.py
@@ -2,4 +2,12 @@
 
 import sys
 
-print(f'hello nice {sys.argv[1]}!')
+def hello_message(name):
+    return f'Hello nice {name}'
+
+def display(msg):
+    print(msg)
+    
+if __name__ == '__main__':
+    msg = hello_message(sys.argv[1])
+    display(msg)
