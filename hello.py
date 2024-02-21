6841f0740adbb6d1ffdcd0750d0a86e9c654698e
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-02-21 16:21:02 +0100

    Introduce command line argument handling

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 
-print(f'hello nice {sys.argv[1]}!')
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('name', type=str, default='world',
+                        help='name to greet')
+args = arg_parser.parse_args()
+print(f'hello nice {args.name}!')
